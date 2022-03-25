8891414d7169585d24ea9c848fd1723924a68974
Chreyd <chreydmapeba@gmail.com>
2022-03-25 23:15:49 +0100

    MainContent1

---
+++ src/components/Earnings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+import {IoStatsChart} from "react-icons/io5"
+import {themeColor,hoverEffect} from "../utils";
+
+function Earnings() {
+  return (
+    <div>Earnings</div>
+  )
+}
+
+export default Earnings
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from 'react';
+import Navbar from "./Navbar";
+import styled from 'styled-components';
 
 function MainContent() {
   return (
-    <div>MainContent</div>
-  )
+    <Container>
+      <Navbar/>
+      <SubContainer>
+        <SectionOne>
+          <ColumnOne1>
+          </ColumnOne1>
+          <ColumnOne2></ColumnOne2>
+        </SectionOne>
+        <SectionTwo>
+          <ColumnOne2></ColumnOne2>
+          <ColumnTwo2></ColumnTwo2>
+        </SectionTwo>
+      </SubContainer>
+    </Container>
+  );
 }
 
+const Container = styled.div`
+  width: 80%;
+  background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%);
+  border-bottom-left-radius: 2rem;
+  border-top-left-radius: 4rem;
+  margin: 1rem 8rem 1rem 4rem;
+ 
+`;
+ 
+const SubContainer = styled.div`
+  margin: 0.5rem 0;
+  height: 80%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+`;
+const TitleText = styled.h3`
+  height: 20%;
+
+`;
+ 
+const SectionOne = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 40%;
+  gap: 2rem;
+  width: 100%;
+`;
+ 
+const ColumnOne1 = styled.div`
+  display: flex;
+  gap: 3rem;
+`;
+ 
+const ColumnTwo1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 115%;
+  width: 100%;
+`;
+ 
+const SectionTwo = styled.div`
+  display: flex;
+  gap: 2rem;
+  height: 26vh;
+`;
+ 
+const ColumnOne2 = styled.div`
+
+`;
+ 
+const ColumnTwo2 = styled.div`
+
+`;
+
+ 
 export default MainContent
